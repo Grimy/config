fbdcf4a5a432108ba5093fe85eff2ec90c757ef5
Victor Adam <Victor.Adam@derpymail.org>
2014-01-16 16:16:57 +0100

    Create all symlinks in install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+ln -s ~/.vim/scripts/.vimperatorrc ~/.vimperatorrc
+ln -s ~/.vim/scripts/.Xresources   ~/.Xresources
+ln -s ~/.vim/scripts/.gitconfig    ~/.gitconfig
+ln -s ~/.vim/scripts/.vimperatorrc ~/.vimperatorrc
+ln -s ~/.vim/scripts/functions/    ~/.config/fish/functions/
+ln -s ~/.vim/powerline-fonts/      ~/.local/share/fonts
+fc-cache -rv
+
 cd ~/.vim
 mkdir -p bundle cache/swaps cache/backups cache/undos
 git clone https://github.com/gmarik/vundle bundle/vundle
-ln -s ~/.vim/powerline-fonts/ ~/.local/share/fonts
 hg clone https://vim.googlecode.com/hg/ src
 
 cd src
@@ -14,10 +21,10 @@ hg update
 	--with-features=huge \
 	--enable-multibyte \
 	--enable-gui=gtk2 \
-	--enable-rubyinterp \
-	--enable-pythoninterp \
-	--enable-python3interp \
 	--enable-perlinterp \
+	# --enable-rubyinterp \
+	--enable-pythoninterp \
+	# --enable-python3interp \
 	--enable-luainterp --with-lua-prefix=/usr --with-luajit \
 	--enable-gpm \
 	--enable-cscope \
