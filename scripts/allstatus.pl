e0aa49fbcdd171bbbba350ed130466be01fa0844
Victor Adam <Victor.Adam@derpymail.org>
2014-01-16 17:38:56 +0100

    Script to show all uncommited or unpushed changes

---
+++ scripts/allstatus.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+my $base = '/home/grimy/.vim';
+chdir $base;
+
+for my $dir ('', <bundle/*>) {
+	chdir "$base/$dir";
+	`git ls-remote --get-url origin` =~ /Grimy/ || next;
+	$_ = `git status --porcelain; git diff --name-only -t HEAD..origin/master`;
+	$_ && print "\t", $dir || 'vimfiles', "\n$_";
+}
