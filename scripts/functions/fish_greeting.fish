e72f6414a88186e846c86cf7f7646d8de669fe32
Victor Adam <Victor.Adam@derpymail.org>
2014-01-16 16:18:16 +0100

    Run ssh-agent on startup

--- scripts/functions/fish_greeting.fish
+++ scripts/functions/fish_greeting.fish
@@ -1,7 +1,14 @@
 function fish_greeting
-	set -gx PATH     $PATH ~/bin
-	set -gx LANG     en_US.UTF-8
-	set -gx EDITOR   vim
-	set -gx MANPAGER "vim -c 'setfiletype man' -"
+	set -gx PATH          $PATH ~/bin
+	set -gx LANG          en_US.UTF-8
+	set -gx EDITOR        vim
+	set -gx MANPAGER      "vim -c 'setfiletype man' -"
+	set -gx SSH_AUTH_SOCK /tmp/ssh-agent
+	set -Ux SSH_AGENT_PID (pidof ssh-agent)
+	if [ -z $SSH_AGENT_PID ]
+		ssh-agent -a $SSH_AUTH_SOCK >/dev/null
+		ssh-add
+	end
+
 	echo 'Howdy!'
 end
