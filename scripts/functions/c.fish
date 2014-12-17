eb1372bc72f8272159d5218095175820d439ac77
Victor Adam <Victor.Adam@derpymail.org>
2014-12-17 18:58:18 +0100

    Compatibility fixes

--- scripts/functions/c.fish
+++ scripts/functions/c.fish
@@ -1,13 +1,13 @@
 function c
 	cd /tmp >/dev/null
-	set CCFLAGS $CCFLAGS -xc -std=c99 -Wall -Wextra -pedantic -Werror -Wno-unused -march=native -fsanitize=address,undefined -g
+	set CFLAGS $CFLAGS -xc -std=c99 -Wall -Wextra -pedantic -Werror -Wno-unused -march=native -g
 
 	perl -le 'print for <#include<{string,std{lib,io}}.h>>' >tmp.c
 	echo 'int main(void) {' >>tmp.c
 	echo 'return 0; }' >>tmp.c
 
 	while read -p 'echo "C> "' C
-		if sed "\$i$C;" tmp.c | clang $CCFLAGS -
+		if sed "\$i$C;" tmp.c | gcc $CFLAGS -
 			printf "br %d\nr\np %s" (wc -l <tmp.c) $C |\
 				gdb -q ./a.out 2>&1 | sed '/quit/Q; s/(gdb) $1 = //; s/(gdb) A syntax error.*/OK/; 1,6d'
 			sed -i "\$i$C;" tmp.c
--- scripts/ycm.py
+++ scripts/ycm.py
@@ -1,39 +1,15 @@
 #!/usr/bin/env python
 
-import os
-# import ycm_core
+# '-I/home/grimy/Core/TesteurSDK/Produits/QT-FC5/include',
+# '-I/home/grimy/Core/TesteurSDK/Core/IHMQT',
+# '-I/home/grimy/Core/TesteurSDK/Core/NoyauIGC/',
 
-FLAGS = [
-	'-Wall', '-Wextra', '-Werror',
-	'-std=c++03', '-x', 'c++',
-	'-I/home/grimy/Core/TesteurSDK/Produits/QT-FC5/include',
-	'-I/home/grimy/Core/TesteurSDK/Core/IHMQT',
-	'-I/home/grimy/Core/TesteurSDK/Core/NoyauIGC/',
-]
-
-# See http://clang.llvm.org/docs/JSONCompilationDatabase.html
-# DATABASE = ycm_core.CompilationDatabase('') if os.path.exists('') else None
-
-# def GetCompilationInfoForFile(filename):
-# 	extension = os.path.splitext(filename)[1]
-# 	if extension in ['.h', '.hxx', '.hpp', '.hh']:
-# 		basename = os.path.splitext(filename)[0]
-# 		for extension in ['.cpp', '.cxx', '.cc', '.c', '.m', '.mm']:
-# 			replacement_file = basename + extension
-# 			if os.path.exists(replacement_file):
-# 				compilation_info = DATABASE.GetCompilationInfoForFile(
-# 					replacement_file)
-# 				if compilation_info.compiler_flags_:
-# 					return compilation_info
-# 		return None
-# 	return DATABASE.GetCompilationInfoForFile(filename)
-
-def DirectoryOfThisScript():
-	return os.path.dirname(os.path.abspath(__file__))
-
-# This is the entry point; this function is called by ycmd to produce flags for a file.
 def FlagsForFile(filename, **kwargs):
-	return {
-		'flags': FLAGS,  # GetCompilationInfoForFile(filename).compiler_flags_ if DATABASE else FLAGS,
-		'do_cache': True,
-	}
+	do_cache = True
+	flags = ['-Weverything', '-Wno-gnu', '-Wno-unused-macros']
+	flags += {
+		'c': ['-std=c99', '-xc'],
+		'cpp': ['-xc++',  '-std=c++11', '-stdlib=libc++']
+	}[filename.rsplit('.', 1)[1]]
+	# flags += ['-I' + dir for dir in
+	return locals()
