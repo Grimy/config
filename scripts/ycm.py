941d3d7fc21bc9340faf6d49a75365181d9e7001
Victor Adam <Victor.Adam@derpymail.org>
2014-12-18 01:55:43 +0100

    Support for project-level compiler flags

--- scripts/ycm.py
+++ scripts/ycm.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
+import subprocess
 # '-I/home/grimy/Core/TesteurSDK/Produits/QT-FC5/include',
 # '-I/home/grimy/Core/TesteurSDK/Core/IHMQT',
 # '-I/home/grimy/Core/TesteurSDK/Core/NoyauIGC/',
 
 def FlagsForFile(filename, **kwargs):
-	do_cache = True
-	flags = ['-Weverything', '-Wno-gnu', '-Wno-unused-macros']
+	do_cache = False
+	flags = ['-Weverything', '-Wno-newline-eof']
 	flags += {
 		'c': ['-std=c99', '-xc'],
 		'cpp': ['-xc++',  '-std=c++11', '-stdlib=libc++']
 	}[filename.rsplit('.', 1)[1]]
-	# flags += ['-I' + dir for dir in
+	flags += subprocess.Popen(
+		'grep CFLAGS $(git rev-parse --show-toplevel) | grep CFLAGS Makefile | cut -d= -f2 | head -n1',
+		shell=True,
+		stdout=subprocess.PIPE).communicate()[0].split(' ')
 	return locals()
