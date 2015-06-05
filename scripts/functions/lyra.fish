b3b40bf0aaf88e3db830f219083d54c097813433
Victor Adam <victor@drawall.cc>
2015-06-05 07:54:47 +0200

    Derpy hooves

--- scripts/functions/lyra.fish
+++ scripts/functions/lyra.fish
@@ -2,5 +2,6 @@ function lyra
 	cd ~/Music
 	find . -name '*.mp3' -or -name '*.wma ' -or -name '*.flac' -or -name '*.m4a' -or -name '*.wav' >playlist
 	mkfifo /tmp/mplayer.fifo
+	ponysay -oqlyra
 	mplayer -shuffle -playlist playlist -idle -slave -input file=/tmp/mplayer.fifo
 end
