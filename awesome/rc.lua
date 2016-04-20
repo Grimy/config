67db366f2c7588c17ab487c4684ad98f8e08e49a
Victor Adam <victor.adam@engie.com>
2016-04-20 14:43:02 +0200

    Très magnifique

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,42 +1,42 @@
-awful = require("awful")
-awful.rules = require("awful.rules")
 require("awful.autofocus")
+aw = require("awful")
+rules = require("awful.rules")
 
 super = {"Mod4"}
 shift = {"Mod4", "Shift"}
-layouts = {awful.layout.suit.max.fullscreen, awful.layout.suit.tile}
+layouts = {aw.layout.suit.tile, aw.layout.suit.magnifier}
 tags = {
     'a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
     'q', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm',
     'w', 'x', 'c', 'v', 'b', 'n'
 }
-for s = 1, screen.count() do awful.tag(tags, s, layouts[1]) end
 
-root.keys(awful.util.table.join(
-    awful.key(super, "Left",   awful.tag.viewprev),
-    awful.key(super, "Right",  awful.tag.viewnext),
-    awful.key(super, "*",      awful.client.movetoscreen),
-    awful.key(super, ":",      awful.client.floating.toggle),
-    awful.key(shift, "Escape", function() client.focus:kill() end),
-    awful.key(super, "Tab",    function() awful.client.focus.byidx( 1); client.focus:raise() end),
-    awful.key(shift, "Tab",    function() awful.client.focus.byidx(-1); client.focus:raise() end),
-    awful.key(super, "Return", function() awful.util.spawn('xterm') end),
-    awful.key(super, "&",      function() awful.widget.prompt():run() end),
-    awful.key(super, "space",  function() awful.layout.inc(layouts, 1) end)))
+function tag(i) return aw.tag.gettags(mouse.screen)[i] end
+for s = 1, screen.count() do aw.tag(tags, s, layouts[1]) end
 
-for i = 1, 26 do
-    root.keys(awful.util.table.join(root.keys(),
-        awful.key(super, tags[i], function() awful.tag.viewonly(awful.tag.gettags(mouse.screen)[i]) end),
-        awful.key(shift, tags[i], function() awful.client.movetotag(awful.tag.gettags(mouse.screen)[i]) end)))
+root.keys(aw.util.table.join(
+    aw.key(super, "Left",      aw.tag.viewprev),
+    aw.key(super, "Right",     aw.tag.viewnext),
+    aw.key(super, "*",         aw.client.movetoscreen),
+    aw.key(shift, "BackSpace", awesome.restart),
+    aw.key(super, "Escape",    function() client.focus:kill() end),
+    aw.key(super, "Tab",       function() aw.client.focus.byidx( 1) end),
+    aw.key(shift, "Tab",       function() aw.client.focus.byidx(-1) end),
+    aw.key(super, "Return",    function() aw.util.spawn('xterm') end),
+    aw.key(super, "&",         function() aw.widget.prompt():run() end),
+    aw.key(shift, "<",         function() aw.tag.incmwfact( 0.05) end),
+    aw.key(super, "<",         function() aw.tag.incmwfact(-0.05) end),
+    aw.key(super, "space",     function() aw.layout.inc(layouts, 1) end)))
+
+for i = 1, #tags do
+    root.keys(aw.util.table.join(root.keys(),
+        aw.key(super, tags[i], function() aw.tag.viewonly(tag(i)) end),
+        aw.key(shift, tags[i], function() aw.client.movetotag(tag(i)) end)))
 end
 
-awful.rules.rules = {{rule = {}, properties = {
+rules.rules = {{rule = {}, callback = aw.client.setslave, properties = {
     border_width = 0,
-    raise = true,
-    buttons = awful.util.table.join(
-        awful.button({},    1, function(c) client.focus = c end),
-        awful.button(super, 1, awful.mouse.client.move),
-        awful.button(super, 3, awful.mouse.client.resize)),
+    focus = true,
+    buttons = aw.button({}, 1, function(c) client.focus = c end),
 }}}
-
-client.connect_signal("manage", function(c) awful.client.setslave(c); client.focus = c end)
+client.connect_signal("focus", function(c) c:raise() end)
--- nvim/syntax/lua.vim
+++ nvim/syntax/lua.vim
@@ -1,2 +1,5 @@
 Comments --
 Flow if|function|for else end
+
+syn keyword Flow do return
+syn region String matchgroup=Normal start=/\v\z(['"])/ end=/\v\z1/
